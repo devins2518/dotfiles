6dab7f6276fdbae88db1530958716e8d3b440e3d
Devin Singh <drsingh2518@icloud.com>
2020-12-12 09:06:23 -0600

    surface init commit

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,10 +1,8 @@
-# Compositor
-picom --experimental-backends --backend glx &
-# Background and colorscheme setter
-feh --bg-fill $HOME/.config/wallpaper/earlymorning.png &
-# Nvidia Fan Controller
-nvfancontrol &
-# Alt tab daemon
-alttab -w 1 -i 128x128 &
 # Top bar
 ~/.config/polybar/launch.sh &
+# Background and colorscheme setter
+feh --bg-fill $HOME/.config/wallpaper/ &
+# Compositor
+picom --experimental-backends --backend glx &
+# Alt tab daemon
+alttab -w 1 -i 128x128 &
--- .config/bspwm/colors.sh
+++ .config/bspwm/colors.sh
@@ -1,21 +1,16 @@
-## Base16 bspwm
-# Author: devins2518
-
-# Colors
-color0=#0E1827
-color1=#284F9D
-color2=#3558A9
-color3=#56619E
-color4=#9076AE
-color5=#688BD0
-color6=#f0a8d7
-color7=#e8c9cf
-color8=#a28c90
-color9=#284F9D
-color10=#3558A9
-color11=#56619E
-color12=#9076AE
-color13=#688BD0
-color14=#B19ABA
-color15=#e8c9cf
-
+#03130D
+#E32613
+#A34B2B
+#DF561B
+#965649
+#D1544B
+#DB9F48
+#97c8cb
+#698c8e
+#E32613
+#A34B2B
+#DF561B
+#965649
+#D1544B
+#DB9F48
+#97c8cb
