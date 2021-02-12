b1b20a5e26f15574de081e3fdaf70868bd72450d
Devin Singh <drsingh2518@icloud.com>
2021-02-11 21:31:05 -0600

    update

--- .config/nvim/lua/rust.lua
+++ .config/nvim/lua/rust.lua
@@ -1,5 +1,9 @@
 g['rustfmt_autosave'] = 1
 g['cargo_shell_command_runner'] = "!"
+map('n', '<Space>cc', '<Nop>')                                 -- Allow jk to escape cmd mode
+map('n', '<Space>cl', '<Nop>')                                 -- Allow jk to escape cmd mode
+map('n', '<Space>cb', '<Nop>')                                 -- Allow jk to escape cmd mode
+map('n', '<Space>cr', '<Nop>')                                 -- Allow jk to escape cmd mode
 map('n', '<leader>cc', ':Ccheck<CR>')                                 -- Allow jk to escape cmd mode
 map('n', '<leader>cl', ':Cclean<CR>')                                 -- Allow jk to escape cmd mode
 map('n', '<leader>cb', ':Cbuild<CR>')                                 -- Allow jk to escape cmd mode
