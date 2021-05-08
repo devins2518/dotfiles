2ff485a4b454a50caaffc3c4f052cd7ec2120b31
Devin Singh <drsingh2518@icloud.com>
2021-05-08 06:32:00 -0500

    update

---
+++ .config/scripts/keybinds.sh
@@ -0,0 +1,7 @@
+sleep 5
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Super>1']" &
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Super>2']" &
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Super>3']" &
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Super>4']" &
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>5']" &
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>6']" &
