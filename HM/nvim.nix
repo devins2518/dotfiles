748dd917273f391f39c038b9cf67b297ffdf00f1
Devin Singh <drsingh2518@icloud.com>
2021-06-06 07:34:33 -0500

    update

--- HM/nvim.nix
+++ HM/nvim.nix
@@ -11,24 +11,24 @@ in {
   home.sessionVariables = { EDITOR = "${package}/bin/nvim"; };
   home.file.".config/nvim".source = ./nvim;
   home.file.".config/nvim".recursive = true;
-  home.file.".config/nvim/coc-settings.json".text = ''
-    {
-       "languageserver": {
-           "zls" : {
-               "command": "zls",
-               "filetypes": ["zig"]
-           },
-            "rnix-lsp" : {
-                "command": "rnix-lsp",
-                "filetypes": ["nix"]
-            }
-       }
-    }'';
+  #home.file.".config/nvim/coc-settings.json".text = ''
+    #{
+       #"languageserver": {
+           #"zls" : {
+               #"command": "zls",
+               #"filetypes": ["zig"]
+           #},
+            #"rnix-lsp" : {
+                #"command": "rnix-lsp",
+                #"filetypes": ["nix"]
+            #}
+       #}
+    #}'';
 
   programs.neovim = {
     enable = true;
     package = package;
-    withNodeJs = true;
+    withNodeJs = false;
     withRuby = false;
     viAlias = true;
     vimAlias = true;
@@ -95,17 +95,14 @@ in {
     '';
     plugins = with pkgs.vimPlugins; [
 
-      vim-nix
-      vim-surround
-      vim-easyescape
       edge
       markdown-preview-nvim
+      vim-easyescape
       vim-go
+      vim-nix
+      vim-startuptime
+      vim-surround
 
-      {
-        plugin = nvim-bufferline-lua;
-        config = "luafile $HOME/.config/nvim/bufferline.lua";
-      }
       {
         plugin = nvim-colorizer-lua;
         config = ''
@@ -114,6 +111,21 @@ in {
           set termguicolors
           lua require "colorizer".setup()
         '';
+      }
+      {
+        plugin = nvim-treesitter;
+        config = luaConfig ''
+          require'nvim-treesitter.configs'.setup {
+            ensure_installed = { "go", "gomod", "lua", "nix", "rust", "toml", "yaml", "zig" },
+            highlight = {
+              enable = true,              -- false will disable the whole extension
+            },
+          }
+        '';
+      }
+      {
+        plugin = nvim-bufferline-lua;
+        config = "luafile $HOME/.config/nvim/bufferline.lua";
       }
       {
         plugin = gitsigns-nvim;
@@ -141,15 +153,16 @@ in {
           nmap <leader>gf :diffget //2<CR>
         '';
       }
-      #{
-      #  plugin = nvim-compe;
-      #  config = "luafile $HOME/.config/nvim/nvim-compe.lua";
+      {
+        plugin = nvim-compe;
+        config = luaConfig ''
+
+        '';
       #}
       #{
       #  plugin = nvim-lspconfig;
       #  config = "luafile $HOME/.config/nvim/nvim-lspconfig/lua.lua";
       #}
-      # broken :(
       {
         plugin = nvim-tree-lua;
         config = "luafile $HOME/.config/nvim/nvimTree.lua";
@@ -172,10 +185,10 @@ in {
           nmap <leader>cl :Cclean<CR>
         '';
       }
-      #{
-      #plugin = nvim-autopairs;
-      #config = ''lua require("nvim-autopairs").setup()'';
-      #}
+      {
+        plugin = nvim-autopairs;
+        config = ''lua require("nvim-autopairs").setup()'';
+      }
       {
         plugin = galaxyline-nvim;
         config = "luafile $HOME/.config/nvim/statusline.lua";
@@ -185,81 +198,73 @@ in {
         config = "luafile $HOME/.config/nvim/web-devicons.lua";
       }
       {
-        plugin = sonokai;
-        config = ''
-          let g:sonokai_style = 'shusia'
-          let g:sonokai_enable_italic = 1
-        '';
-      }
-      {
         plugin = indentLine;
         config = ''
           let g:indentLine_enabled = 1
           let g:indentLine_char_list = ['|', '¦', '┆', '┊']
         '';
       }
-      {
-        plugin = coc-nvim;
-        config = ''
-          " coc config
-          let g:coc_global_extensions = [
-            \ "coc-clangd",
-            \ "coc-discord-neovim",
-            \ "coc-go",
-            \ "coc-html",
-            \ "coc-json",
-            \ "coc-pairs",
-            \ "coc-prettier",
-            \ "coc-python",
-            \ "coc-rust-analyzer",
-            \ "coc-sh",
-            \ "coc-snippets",
-            \ "coc-toml",
-            \ ]
+      #{
+        #plugin = coc-nvim;
+        #config = ''
+          #" coc config
+          #let g:coc_global_extensions = [
+            #\ "coc-clangd",
+            #\ "coc-discord-neovim",
+            #\ "coc-go",
+            #\ "coc-html",
+            #\ "coc-json",
+            #\ "coc-pairs",
+            #\ "coc-prettier",
+            #\ "coc-python",
+            #\ "coc-rust-analyzer",
+            #\ "coc-sh",
+            #\ "coc-snippets",
+            #\ "coc-toml",
+            #\ ]
 
-          " Use <C-k> for jump to previous placeholder, it"s default of coc.nvim
-          let g:coc_snippet_prev = "<c-k>"
+          #" Use <C-k> for jump to previous placeholder, it"s default of coc.nvim
+          #let g:coc_snippet_prev = "<c-k>"
 
-          " Rename symbol
-          nmap <leader>rn <Plug>(coc-rename)
+          #" Rename symbol
+          #nmap <leader>rn <Plug>(coc-rename)
 
-          inoremap <silent><expr> <TAB>
-                \ pumvisible() ? coc#_select_confirm() :
-                \ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump','''])\<CR>" :
-                \ <SID>check_back_space() ? "\<TAB>" :
-                \ coc#refresh()
+          #inoremap <silent><expr> <TAB>
+                #\ pumvisible() ? coc#_select_confirm() :
+                #\ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump','''])\<CR>" :
+                #\ <SID>check_back_space() ? "\<TAB>" :
+                #\ coc#refresh()
 
-          function! s:check_back_space() abort
-            let col = col('.') - 1
-            return !col || getline('.')[col - 1]  =~# '\s'
-          endfunction
+          #function! s:check_back_space() abort
+            #let col = col('.') - 1
+            #return !col || getline('.')[col - 1]  =~# '\s'
+          #endfunction
 
-          " from readme
-          " if hidden is not set, TextEdit might fail.
-          set hidden " Some servers have issues with backup files, see #649 set nobackup set nowritebackup " Better display for messages set cmdheight=2 " You will have bad experience for diagnostic messages when it"s default 4000.
-          set updatetime=300
+          #" from readme
+          #" if hidden is not set, TextEdit might fail.
+          #set hidden " Some servers have issues with backup files, see #649 set nobackup set nowritebackup " Better display for messages set cmdheight=2 " You will have bad experience for diagnostic messages when it"s default 4000.
+          #set updatetime=300
 
-          " GoTo code navigation.
-          nmap <silent> gd <Plug>(coc-definition)
-          nmap <silent> gy <Plug>(coc-type-definition)
-          nmap <silent> gi <Plug>(coc-implementation)
-          nmap <silent> gr <Plug>(coc-references)
-          nnoremap <silent> K :call <SID>show_documentation()<CR>
-          nnoremap <silent> gh :call <SID>show_documentation()<CR>
-          inoremap <silent><expr> <c-space> coc#refresh()
-          nnoremap <silent> gb <C-^>
+          #" GoTo code navigation.
+          #nmap <silent> gd <Plug>(coc-definition)
+          #nmap <silent> gy <Plug>(coc-type-definition)
+          #nmap <silent> gi <Plug>(coc-implementation)
+          #nmap <silent> gr <Plug>(coc-references)
+          #nnoremap <silent> K :call <SID>show_documentation()<CR>
+          #nnoremap <silent> gh :call <SID>show_documentation()<CR>
+          #inoremap <silent><expr> <c-space> coc#refresh()
+          #nnoremap <silent> gb <C-^>
 
-          " Show documentation
-          function! s:show_documentation()
-            if &filetype == "vim"
-              execute "h ".expand("<cword>")
-            else
-              call CocAction("doHover")
-            endif
-          endfunction
-        '';
-      }
-
+          #" Show documentation
+          #function! s:show_documentation()
+            #if &filetype == "vim"
+              #execute "h ".expand("<cword>")
+            #else
+              #call CocAction("doHover")
+            #endif
+          #endfunction
+        #'';
+      #}
     ];
   };
 }
