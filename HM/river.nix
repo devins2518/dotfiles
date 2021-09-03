51e3ff22951a438d7ad9bdf622e2d3e9a4bc3350
Devin Singh <drsingh2518@icloud.com>
2021-09-03 18:09:45 -0500

    update

--- HM/river.nix
+++ HM/river.nix
@@ -10,7 +10,8 @@ in rec {
     #!/usr/bin/env bash
 
     riverctl spawn "wlr-randr --output eDP-1 --scale 2 &"
-    riverctl spawn "paper -i /etc/wallpaper/wallpaper.png"
+    riverctl spawn "paper -c '#FF282828'"
+    # riverctl spawn "paper -i /etc/wallpaper/wallpaper.png"
     # riverctl spawn 'swaybg -c "#282828"'
     riverctl spawn 'kile'
     riverctl spawn 'mako'
