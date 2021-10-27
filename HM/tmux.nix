a03241f3ac9e7d5a33c511bb3cb3c8d3d4e65576
Devin Singh <drsingh2518@icloud.com>
2021-10-26 22:48:20 -0500

    update

--- HM/tmux.nix
+++ HM/tmux.nix
@@ -111,20 +111,26 @@ in {
         # bell
         set-window-option -g window-status-bell-style bg=colour167,fg=colour235 # bg=red, fg=bg
 
-        ## Theme settings mixed with colors (unfortunately, but there is no cleaner way)
-        set-option -g status-justify "left"
-        set-option -g status-left-style none
-        set-option -g status-left-length "80"
-        set-option -g status-right-style none
-        set-option -g status-right-length "80"
-        set-window-option -g window-status-separator ""
-
-        set-option -g status-left "#[bg=colour241,fg=colour248] #S #[bg=colour237,fg=colour241,nobold,noitalics,nounderscore]"
-        set-option -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] %Y-%m-%d %H:%M #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237] #h "
-
-        set-window-option -g window-status-current-format "#[bg=colour214,fg=colour237,nobold,noitalics,nounderscore]#[bg=colour214,fg=colour239] #I#[bg=colour214,fg=colour239,bold] #W#{?window_zoomed_flag,*Z,} #[bg=colour237,fg=colour214,nobold,noitalics,nounderscore]"
-        set-window-option -g window-status-format "#[bg=colour239,fg=colour237,noitalics]#[bg=colour239,fg=colour223] #I#[bg=colour239,fg=colour223] #W #[bg=colour237,fg=colour239,noitalics]"
-              '';
+        set -g mode-style "fg=#719cd6,bg=#3b4261"
+        set -g message-style "fg=#719cd6,bg=#3b4261"
+        set -g message-command-style "fg=#719cd6,bg=#3b4261"
+        set -g pane-border-style "fg=#3b4261"
+        set -g pane-active-border-style "fg=#719cd6"
+        set -g status "on"
+        set -g status-justify "left"
+        set -g status-style "fg=#719cd6,bg=#131A24"
+        set -g status-left-length "100"
+        set -g status-right-length "100"
+        set -g status-left-style NONE
+        set -g status-right-style NONE
+        set -g status-left "#[fg=#393b44,bg=#719cd6,bold] #S #[fg=#719cd6,bg=#131A24,nobold,nounderscore,noitalics]"
+        set -g status-right "#[fg=#131A24,bg=#131A24,nobold,nounderscore,noitalics]#[fg=#719cd6,bg=#131A24] #{prefix_highlight} #[fg=#3b4261,bg=#131A24,nobold,nounderscore,noitalics]#[fg=#719cd6,bg=#3b4261] %Y-%m-%d  %I:%M %p #[fg=#719cd6,bg=#3b4261,nobold,nounderscore,noitalics]#[fg=#393b44,bg=#719cd6,bold] #h "
+        setw -g window-status-activity-style "underscore,fg=#AEAFB0,bg=#131A24"
+        setw -g window-status-separator ""
+        setw -g window-status-style "NONE,fg=#AEAFB0,bg=#131A24"
+        setw -g window-status-format "#[fg=#131A24,bg=#131A24,nobold,nounderscore,noitalics]#[default] #I  #W #F #[fg=#131A24,bg=#131A24,nobold,nounderscore,noitalics]"
+        setw -g window-status-current-format "#[fg=#131A24,bg=#3b4261,nobold,nounderscore,noitalics]#[fg=#719cd6,bg=#3b4261,bold] #I  #W #F #[fg=#3b4261,bg=#131A24,nobold,nounderscore,noitalics]"
+      '';
     };
   };
 }
